0844e480944f2dba33f10773074b314766f342ce
ANP2026 <abhimanyuparmar2013@gmail.com>
2026-05-30 12:09:51 -0400

    Add files via upload

---
+++ Star.py
@@ -0,0 +1,21 @@
+import turtle 
+turtle.Screen().bgcolor("Orange")
+board=turtle.Turtle()
+board.forward(200)
+board.left(120)
+board.forward(200)
+board.left(120)
+board.forward(200)
+
+board.penup()
+board.right(150)
+board.forward(100)
+
+board.pendown()
+board.right(90)
+board.forward(200)
+board.right(120)
+board.forward(200)
+board.right(120)
+board.forward(200)
+turtle.done()
